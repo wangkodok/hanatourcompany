871a7222944434b6fd00ad37da675f501dc576f2
wangkodok <wangkodok@gmail.com>
2025-03-24 15:01:55 +0900

    fix：비주얼 슬라이더 영상 재생/정지 버튼 동작 오류 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,4 +17,76 @@ window.addEventListener("load", () => {
       },
     },
   });
+
+  // 비주얼 슬라이더 버튼 클릭 시 영상 재생/정지 버튼
+  const visualSlide = document.querySelectorAll("#visual-slide .swiper-slide");
+  const paginationBullets = document.querySelectorAll('.swiper-pagination button');
+  const videoPlayer = document.querySelector("#video-player");
+  const video = document.querySelectorAll(".video");
+  
+  const buttonActive = "swiper-pagination-bullet-active";
+  paginationBullets.forEach((buttonItem) => {
+    buttonItem.addEventListener("click", () => {
+      if (!buttonItem.classList.contains(buttonActive)) {
+        console.log(buttonItem, "buttonItem");
+        visualSlide.forEach((item) => {
+          item.querySelector(".video").play();
+          videoPlayer.classList.remove("active");
+        })
+      };
+    });
+  });
+  
+  videoPlayer.addEventListener("click", () => {
+    visualSlide.forEach((item) => {
+      if (item.classList.contains("swiper-slide-active")) {
+        // console.log(item.querySelector(".video").pause());
+        if (item.querySelector(".video").paused || item.querySelector(".video").ended) {
+          item.querySelector(".video").play(); // 재생
+          videoPlayer.classList.remove("active");
+        } else {
+          item.querySelector(".video").pause(); // 정지
+          videoPlayer.classList.add("active");
+        }
+        // console.log(item.classList.contains("swiper-slide-active"));
+      };
+    });
+  });
+
+  // 각 버튼 안에 있는 .blind 요소 찾기
+  // paginationBullets.forEach((item) => {
+  //   item.addEventListener("click", (e) => {
+  //     if (videoPlayer.classList.contains("active")) {
+  //       console.log(item, "item 아이템");
+  //       videoPlayer.classList.add("active");
+  //     }
+  //     video.forEach((videoItem) => {
+  //       videoItem.play(); // 재생
+  //       videoPlayer.classList.remove("active");
+  //     });
+  //   });
+  // });
+
+
+
+  // video.forEach((item) => {
+  //   let isVideoPlayButton = true;
+  //   videoPlayer.addEventListener("click", function () {
+  //     // if (item.paused || item.ended) {
+  //     //   item.play(); // 재생
+  //     // } else {
+  //     //   item.pause(); // 정지
+  //     // }
+
+  //     if (isVideoPlayButton) {
+  //       item.pause(); // 정지
+  //       videoPlayer.classList.add("active");
+  //       isVideoPlayButton = false;
+  //     } else {
+  //       item.play(); // 재생
+  //       videoPlayer.classList.remove("active");
+  //       isVideoPlayButton = true;
+  //     }
+  //   });
+  // });
 });
